99ea92e9a70104c264443633b8b7b112e21807e4
Nitish Jha <njkrox16@gmail.com>
2022-12-10 13:03:26 +0530

    backend completed

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js";
 import {register} from "./controllers/auth.js";
 import {createPost} from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users,posts } from "./data/index.js";
 
 
 /*CONFIGURATION*/
@@ -61,6 +64,10 @@ mongoose.connect(process.env.MONGO_URL,{
 }).then(()=>{
     app.listen(PORT, ()=>{
         console.log(`Server running on : ${PORT}`);
+
+        /*ADD DATA ONLY ONCE*/
+        // User.insertMany(users);
+        // Post.insertMany(posts);
     })
 }).catch((error)=>console.log(`${error} did not connect..`))
 
