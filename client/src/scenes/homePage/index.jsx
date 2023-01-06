a3ef94629b931ee3408015e4f27367c28ea97662
Nitish Jha <njkrox16@gmail.com>
2023-01-06 13:19:57 +0530

    Frontend : created Post widget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,6 +1,7 @@
 import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
 import UserWidget from "scenes/widgets/UserWidget";
 
 const HomePage = () => {
@@ -21,8 +22,9 @@ const HomePage = () => {
                 <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
                     <UserWidget userId={_id} picturePath={picturePath} />
                 </Box>
-                <Box flexBasis={isNonMobileScreens ? "26%" : undefined}
+                <Box flexBasis={isNonMobileScreens ? "46%" : undefined}
                     mt={isNonMobileScreens ? undefined : "2rem"}>
+                    <MyPostWidget picturePath={picturePath} />
                 </Box>
                 {isNonMobileScreens && (
                     <Box flexBasis="26%"></Box>
